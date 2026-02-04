dd3490760bfae4f33342221fd9d217d00ef1643c
Mohammed Bashamkha <mb877402@gmail.com>
2026-02-04 12:22:40 +0300

    Refactor API routes: Clean up commented code and maintain essential routes for tasks and categories

--- routes/api.php
+++ routes/api.php
@@ -5,22 +5,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-//
-
-// Route::post('/tasks',[TaskController::class,'store']);  // Create
-
-// Route::put('/tasks/{id}',[TaskController::class,'update']); // Update
-
-// Route::get('/tasks/{id}',[TaskController::class,'show']); // Show or Search
-
-// Route::delete('/tasks/{id}',[TaskController::class,'delete']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
@@ -44,8 +30,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
 
-    // Route::apiResource('/profile',TaskController::class)->middleware('auth:sanctum'); // All-in-one
-
     Route::get('user/{id}/profile',[UserController::class,'getProfile']);
 
 
@@ -60,10 +44,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('tasks/{task_id}/categories',[TaskController::class,'AddCategoriesToTasks']);
 
     // category Routes
-    // Route::get('/category',[CategoryController::class,'index']);
-
-    // Route::post('/category',[CategoryController::class,'store']);
-
     Route::apiResource('/category',CategoryController::class)->middleware('auth:sanctum'); // All-in-one
 
     Route::get('/task/{taskId}/categories',[TaskController::class,'getTaskCategories']);
