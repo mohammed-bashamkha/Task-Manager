e0c35c0180c87de94f9b9420e9e8ecde18ae9c26
Mohammed Bashamkha <mb877402@gmail.com>
2026-02-07 12:16:01 +0300

    Add send-mail route to facilitate email sending functionality for test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-mail',function(){
+    Mail::raw('This is a test email using Laravel 12', function($message){
+        $message->to('user@localhost.test')->subject('Test Email from Laravel 12');
+    });
+    return "Email Sent Successfully";
 });
