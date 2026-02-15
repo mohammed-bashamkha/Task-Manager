65a716a57a4ca680a73b2c4f333ade078f633afa
Mohammed Bashamkha <mb877402@gmail.com>
2026-02-15 13:20:38 +0300

    feat: Integrate Spatie Laravel Permission package for role and permission management, update User model and seeders

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $permissions = [
+            'task.view_all',
+            'task.view_own',
+            'task.create',
+            'task.update',
+            'task.delete',
+            'task.assign',
+            'category.manage',
+            'user.manage',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        $admin = Role::firstOrCreate(['name' => 'admin']);
+        $manager = Role::firstOrCreate(['name' => 'manager']);
+        $user = Role::firstOrCreate(['name' => 'user']);
+
+        $admin->givePermissionTo(Permission::all());
+
+        $manager->givePermissionTo([
+            'task.view_all',
+            'task.create',
+            'task.update',
+            'task.assign',
+        ]);
+
+        $user->givePermissionTo([
+            'task.view_own',
+            'task.update',
+        ]);
+    }
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,6 +11,16 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(30)->create();
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.dev',
+            'password' => bcrypt('password'),
+        ]);
+        $admin->assignRole('admin');
+        
+        $user = User::factory()->count(30)->create();
+        $user->each(function ($user) {
+            $user->assignRole('user');
+        });
     }
 }
